76763eddeecd926fd8486a4f0c0b91ceef5e44c7
Jacup <jacubgr@gmail.com>
2017-03-19 16:16:59 +0100

    up

---
+++ python/2_temat/Fib2.py
@@ -0,0 +1,55 @@
+
+def mult(m1, m2):
+    n = len(m1)
+    tmp = [[0 for i in range(n)] for j in range(n)]
+    val = 0
+    for i in range(n):
+        for j in range(n):
+            for c in range(n):
+                val += m1[i][c] * m2[c][j]
+            tmp[i][j] = val
+            val = 0
+    return tmp
+
+
+def najwiekszaPotega2(n):
+    i = 0
+    za_duzo = False
+    while not za_duzo:
+        if 2**i > n:
+            za_duzo = True
+        else:
+            i += 1
+    return i-1
+
+
+def listaPoteg2(n):
+    if n == 1:
+        return [0]
+    else:
+        k = najwiekszaPotega2(n)
+        wyn = [k]
+        if n == 2 ** k:
+            return wyn
+        else:
+            wyn.extend(listaPoteg2(n-2**k))
+            return wyn
+
+print listaPoteg2(9)
+
+
+def multPow2(a, n):
+    while n > 0:
+        a = mult(a, a)
+        n -= 1
+    return a
+
+
+def fib3(n):
+    a = [[1, 1], [1, 0]]
+    lista_macierzy = []
+
+    for p in listaPoteg2(n):
+        lista_macierzy.append(multPow2(a, p))
+
+    return reduce(mult, lista_macierzy)[0][1]
---
+++ python/2_temat/fib.py
@@ -0,0 +1,50 @@
+import time
+from Fib2 import fib3
+import matplotlib.pylab as plt
+
+
+def fib2(n):
+    f1 = 0
+    f2 = 1
+    k = n - 2
+    if n < 2:
+        f2 = n
+    else:
+        while k >= 0:
+            f2, f1, k = f1 + f2, f2, k-1
+    return f2
+
+
+def fib1(n):
+    if n < 2:
+        return n
+    else:
+        return fib1(n-1) + fib1(n-2)
+
+
+# fibs = [fib1, fib2, fib3]
+fibs = [fib2, fib3]
+
+results = {f.__name__: [] for f in fibs}
+print results
+
+r_begin = 20000
+r_end = 30000
+r_range = range(r_begin, r_end, 5)
+
+for n in r_range:
+    for f in fibs:
+        start_time = time.clock()
+        f(n)
+        results[f.__name__].append(time.clock() - start_time)
+    print "n =", n
+
+print results
+
+
+# plt.plot(r_range, results["fib1"], linestyle='--', color='r')
+plt.plot(r_range, results["fib2"], linestyle='-', color='g')
+plt.plot(r_range, results["fib3"], linestyle='-', color='b')
+plt.show()
+# for (x, y) in [(1, 2), (3, 4)]:
+#     print y
